84bb73cbdb25a7b707487aedb10c1d6aeb1f3822
botatoes <botatoes@gmail.com>
2016-11-02 16:14:51 -0700

    tweek new page and sticky nav

--- js/my.js
+++ js/my.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
   
   $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 608) {
+    if ($(window).scrollTop() > 560) {
       $('#nav_bar').addClass('navbar-fixed');
     }
-    if ($(window).scrollTop() < 609) {
+    if ($(window).scrollTop() < 561) {
       $('#nav_bar').removeClass('navbar-fixed');
     }
   });
